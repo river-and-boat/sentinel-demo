20cb7a8df8c34b57a37eab4c44dd2c6edf8eb0f3
Pengrui Zhao <pengrui.zhaop@thoughtworks.com>
2021-09-03 09:53:16 +0800

    finish init

---
+++ src/main/java/com/beach/sentinelstudy/SentinelStudyApplication.java
@@ -0,0 +1,13 @@
+package com.beach.sentinelstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beach/sentinelstudy/SentinelStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beach.sentinelstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
